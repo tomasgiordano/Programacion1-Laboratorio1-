494e4d98b561aab7a066eb7a3f5a2c066294e09c
tomasgiordano <tomiigiordano@gmail.com>
2019-03-26 11:35:33 -0300

    26/3 fin

--- Clase 4/Bibliotecas/main.c
+++ Clase 4/Bibliotecas/main.c
@@ -16,8 +16,40 @@ int main()
     return 0;
     */
 
-    long long int resultado= factorial(5);
+    /*
+    int resultado= factorial(5);
     printf("%d",resultado);
 
+    return 0;
+    */
+    int opcion;
+    char seguir='s';
+
+    do
+    {
+        printf("1.Alta\n2.Baja\n3.Informar\n4.Salir\nElija una opcion.");
+        scanf("%d", &opcion);
+        switch(opcion)
+        {
+        case 1:
+            printf("Estoy dando de alta!\n");
+        break;
+        case 2:
+            printf("Estoy dando de baja!\n");
+        break;
+        case 3:
+            printf("Estoy informando!\n");
+        break;
+        case 4:
+            printf("Cerrando el programa!\n");
+            seguir='n';
+        break;
+        default:
+            printf("Ingrese una opcion valida!");
+        break;
+        }
+        system("pause");
+        system("cls");
+    }while(seguir=='s');
     return 0;
 }
