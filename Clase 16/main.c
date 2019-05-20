1d1914ab0e592c113d688dca4d2d671d5da25fac
alumno <alumno>
2019-05-20 11:32:20 -0300

    punteros

--- Clase 16/main.c
+++ Clase 16/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct
 {
@@ -7,30 +9,60 @@ typedef struct
     char letra;
 }eDato;
 
-void mostrar(eDato*);
+void mostrar(eDato*,int);
+void mostrarUno(eDato*,int);
+void ordenarPorLetra(eDato*,int);
 
 int main()
 {
-    eDato unDato={1,'C'};
-    eDato* pDato;
-    pDato=&unDato;
-    printf("%d--%c\n",pDato->numero,pDato->letra);
+    //eDato unDato={1,'C'};
+    //eDato* pDato;
+    //pDato=&unDato;
+    //printf("%d--%c\n",pDato->numero,pDato->letra);
 
     eDato muchosDatos[3]={{1,'C'},{1,'B'},{1,'A'}};
-    eDato* pMuchosDatos;
-    pMuchosDatos=muchosDatos;
 
-    mostrar(pMuchosDatos);
+    ordenarPorLetra(muchosDatos,3);
+    mostrar(muchosDatos,3);
+    printf("----------------------\n");
+    mostrarUno(muchosDatos,2);
+    printf("----------------------\n");
 
     return 0;
 }
 
-void mostrar(eDato *pMuchosDatos)
+void mostrar(eDato *pMuchosDatos,int tam)
 {
     int i;
 
-    for(i=0;i<3;i++)
+    for(i=0;i<tam;i++)
     {
-        printf("%d--%c\n",(pMuchosDatos+i)->numero,(pMuchosDatos+i)->letra);
+        mostrarUno(pMuchosDatos,i);
+    }
+}
+
+void mostrarUno(eDato *pMuchosDatos,int indice)
+{
+    printf("%d)%d--%c\n",indice+1,(pMuchosDatos+indice)->numero,(pMuchosDatos+indice)->letra);
+}
+
+void ordenarPorLetra(eDato *pMuchosDatos,int tam)
+{
+    int i;
+    int j;
+
+    eDato copy;
+
+    for(i=0;i<tam-1;i++)
+    {
+        for(j=i+1;j<tam;j++)
+        {
+            if((pMuchosDatos+i)->letra > (pMuchosDatos+j)->letra)
+            {
+                 copy=*(pMuchosDatos+i);
+                *(pMuchosDatos+i)=*(pMuchosDatos+j);
+                *(pMuchosDatos+j)=copy;
+            }
+        }
     }
 }
