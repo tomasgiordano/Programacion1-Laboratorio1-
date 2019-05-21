a59821fd7f255a06f850bbd94637325a6d0a43a3
tomasgiordano <tomiigiordano@gmail.com>
2019-05-21 12:33:47 -0300

    ARRAY NUMEROS

--- Clase 17/main.c
+++ Clase 17/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define ELEMENTS 5
 int main()
 {
     /*****************************--MALLOC-CALLOC-FREE--**********************************/
@@ -32,9 +32,49 @@ int main()
 
     /*********************************************************************************/
 
-    int pNumeros;
+    int* pNumeros;
+    int* pAux;
+    int i;
+    int cont=0;
 
+    pNumeros=(int*) malloc(sizeof(int)*5);
+    pAux=pNumeros;
 
+    if(pNumeros!=NULL)
+    {
+        for(i=0;i<ELEMENTS;i++)
+        {
+            //*(pNumeros+i)=i+1;
+            printf("Ingrese un numero: ");
+            scanf("%d",pNumeros+i);
+        }
 
+        pAux= (int*) realloc(pNumeros,sizeof(int)*(ELEMENTS+5));
+
+        if(pAux!=NULL)
+        {
+            pNumeros= pAux;
+
+            for(i=5;i<ELEMENTS+5;i++)
+            {
+                //*(pNumeros+i)=i+1;
+                printf("Ingrese un numero: ");
+                scanf("%d",pNumeros+i);
+            }
+            for(i=0;i<ELEMENTS+5;i++)
+            {
+                printf("%d\n",(*(pNumeros+i)));
+            }
+            free(pAux);
+        }
+        else
+        {
+            for(i=0;i<ELEMENTS;i++)
+            {
+                printf("%d\n",(*(pNumeros+i)));
+            }
+            free(pAux);
+        }
+    }
     return 0;
 }
