d5587f53cd0f2b9d03fe832ae7fbb18ea6ead06d
tomasgiordano <tomiigiordano@gmail.com>
2019-05-21 10:14:00 -0300

    MALLOC & FREE

---
+++ Clase 17/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*****************************--MALLOC-CALLOC-FREE--**********************************/
+
+    /**<
+    int* pNumero;
+    pNumero = (int*) malloc(sizeof(int));
+    *pNumero=99;
+
+    printf("Heap-->pNumero: %d\n", pNumero);
+    printf("Stack-->&pNumero: %d\n", &pNumero);
+
+    if(pNumero!=NULL)
+    {
+        printf("Valor-->*pNumero: %d\n", *pNumero);
+    }
+
+    free(pNumero);
+    printf("--Luego de hacer el free--");
+    system("pause");
+
+    printf("Heap-->pNumero: %d\n", pNumero);
+    printf("Stack-->&pNumero: %d\n", &pNumero);
+
+    if(pNumero!=NULL)
+    {
+        printf("Valor-->*pNumero: %d\n", *pNumero);
+    }  */
+
+    /*********************************************************************************/
+
+    int pNumeros;
+
+
+
+    return 0;
+}
